a9ca5c3da24d560422ebf7674558dd91aa41c122
Якубова Александра <yakubova_as@mail.ru>
2022-08-21 14:19:55 +0400

    mv ProfileViewController into Profile && upd backgroundColor

---
+++ Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Иван Сёмин on 20.08.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .lightGray
+    }
+}
