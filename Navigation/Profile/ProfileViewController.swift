f1eddf160ae8830b2431c3cb85103c268482b312
Якубова Александра <yakubova_as@mail.ru>
2022-08-21 14:42:43 +0400

    add Subview into ProfileViewController
    fix

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    private lazy var profileHeaderView: ProfileHeaderView = {
+        let view = ProfileHeaderView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        
+        view.backgroundColor = .systemPink
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .lightGray
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        self.view.addSubview(self.profileHeaderView)
     }
 }
